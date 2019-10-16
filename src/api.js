715a5e3181f7da051a5acec5271eb4a8b542e5f1
github username <github email address>
2019-10-16 17:27:16 +0100

    Done.

---
+++ src/api.js
@@ -0,0 +1,49 @@
+
+// CONSTANTS
+
+const baseURL = "http://localhost:3000/quotes/"
+const baseEmbLikesURL = "http://localhost:3000/quotes?_embed=likes"
+const likeURL = "http://localhost:3000/likes"
+const
+
+headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json"
+}
+
+
+// FUNCTIONALITY
+
+function get(){
+    return fetch(baseEmbLikesURL).then(resp => resp.json())
+}
+
+function postLike(quote){
+    return fetch(likeURL, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({
+            quoteId: quote.id
+        })
+    })
+}
+
+function postQuote(newQuote){
+    return fetch(baseURL, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(newQuote)
+    })
+}
+
+
+function deleteQuote(quote){fetch(baseURL+`${quote.id}`, {method: "DELETE"})}
+
+
+
+API = {
+    get: get(),
+    postQuote: postQuote,
+    postLike: postLike,
+    deleteQuote: deleteQuote
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,74 @@
-// It might be a good idea to add event listener to make sure this file 
-// only runs after the DOM has finshed loading. 
+
+document.addEventListener("DOMContentLoaded", renderQuotes())
+
+// CONSTANTS
+
+const quoteList = document.querySelector('#quote-list')
+const newQuoteForm = document.querySelector('#new-quote-form')
+const submitForm = document.querySelector('#new-quote-form')
+
+
+// CORE FUNCTIONALITY
+API.get.then(quotes => console.log(quotes))
+
+function renderQuotes(){
+    API.get.then(quotes => quotes.forEach(q => renderQuote(q)))
+}
+
+function renderQuote(quote){
+    let quoteCard = createElement('li', 'quote-card')
+    quoteCard.id = quote.id
+    let blockquote = createElement('blockquote', 'blockquote')
+
+    let quoteText = createElement('p', 'mb-0', `${quote.quote}`)
+    let author = createElement('footer', 'blockquote-footer', `${quote.author}`)
+    let likeButton = createElement('button', 'btn-success', `Likes: ${quote.likes.length}` )
+    let deleteButton = createElement('button', 'btn-danger', 'Delete' )
+
+    likeButton.addEventListener('click', (e) => likeFunction(e, quote))
+    deleteButton.addEventListener('click', (e) => deleteFunction(e, quote))
+
+    blockquote.append(quoteText, author, likeButton, deleteButton)
+    quoteCard.appendChild(blockquote)
+    quoteList.appendChild(quoteCard)
+}
+
+// BUTTON FUNCTIONALITY
+
+function likeFunction(e, quote){  
+    API.postLike(quote) //be
+    let likes = parseInt(e.target.innerText.split(' ')[1]) //fe
+    e.target.innerText = "Likes: "+`${likes+1}`
+}
+
+function deleteFunction(e, quote){
+    API.deleteQuote(quote) //be
+    document.getElementById(quote.id).remove() //fe
+}
+
+submitForm.addEventListener('submit', (e) => addQuote(e))
+
+function addQuote(e){
+    e.preventDefault()
+    API.postQuote(contentFromForm(e)) //be
+    renderQuote({...contentFromForm(e), likes: []}) //fe - doens't allow for immediate like
+}
+
+function contentFromForm(e){
+    let obj = {
+    quote: e.target.elements[0].value,
+    author: e.target.elements[1].value
+    }
+    return obj
+}
+
+
+
+// HELPER METHODS
+
+function createElement(ele, className="", innerText=""){
+    let element = document.createElement(ele);
+    element.className = className;
+    element.innerText = innerText;
+    return element
+}
